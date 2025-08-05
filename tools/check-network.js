63a62815037363d2243523b0472d866a84b1e1dd
Per Liedman <per@liedman.net>
2025-08-05 11:48:44 +0200

    Improve island checker

--- tools/check-network.js
+++ tools/check-network.js
@@ -1,5 +1,5 @@
 import PathFinder from "geojson-path-finder";
-import network from "../src/data/skaneleden.json" assert { type: "json" };
+import network from "../src/data/skaneleden.json" with { type: "json" };
 
 const pathFinder = new PathFinder.default(network);
 const vertices = Object.keys(pathFinder.graph.compactedVertices).map((id) => ({
@@ -49,9 +49,14 @@ console.log(
     {
       type: "FeatureCollection",
       features: vertices
-        .map(({ properties: { id: startId, islandId } }) =>
-          Object.keys(pathFinder.graph.compactedCoordinates[startId]).map(
-            (endId) => ({
+        .map(({ properties: { id: startId, islandId } }) => {
+          const neighbors = Object.keys(
+            pathFinder.graph.compactedCoordinates[startId]
+          );
+          if (neighbors.length > 0) {
+            return Object.keys(
+              pathFinder.graph.compactedCoordinates[startId]
+            ).map((endId) => ({
               type: "Feature",
               properties: { startId, endId, islandId },
               geometry: {
@@ -59,9 +64,20 @@ console.log(
                 coordinates:
                   pathFinder.graph.compactedCoordinates[startId][endId],
               },
-            })
-          )
-        )
+            }));
+          } else {
+            return [
+              {
+                type: "Feature",
+                properties: { id: startId, islandId },
+                geometry: {
+                  type: "Point",
+                  coordinates: startId.split(",").map(Number),
+                },
+              },
+            ];
+          }
+        })
         .flat(),
     },
     null,
